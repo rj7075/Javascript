5924de1b15b7574d8859d28b3d4d3b3e9e480ba6
rj7075 <128252566+rj7075@users.noreply.github.com>
2024-05-16 04:34:50 +0000

    configration done

--- 01_basics/05_Datatypes_Summary.js
+++ 01_basics/05_Datatypes_Summary.js
@@ -4,3 +4,44 @@
 // Referance (Non primitive Datatypes)
 
 //Array,object,function
+//Javascript is Dynamically typed langauge we no need to define what kind of 
+//data types we assign or store in variable
+const id= Symbol("123");
+const anotherid=Symbol("123");// symbol allways provide a unique id
+console.log(id==anotherid);//false
+
+const array=["Ranjeet","Yadav","Basti","wale","software","Engineer"];
+
+let object ={
+    name:"Ranjeet",
+    Surname:"Yadav",
+}
+
+const myFunction=function(){
+    console.log("Hello Ranjeet Bhai");
+
+}
+//all non primitive types are object 
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Memory types-:Stack memory- Primitive Data stores(WE got ca copy of variable whenever a stack memory is created
+//
+ 
+// Heap memory-:Non-rimitive 
+let myname="Ranjeetyadav";
+ let anothername="ranjeet";
+ anothername="yadav";
+ console.log(myname);
+ console.log(anothername); 
+
+ let userone={
+email:"rj7075yadav@gmail.com" ,
+upi:"user1@ybl"
+ }
+  
+
+
+     let usertwo=userone;
+     usertwo.email="rjeet@gmail.com";
+     console.log(usertwo);
